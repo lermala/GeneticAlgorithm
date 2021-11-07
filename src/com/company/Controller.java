bcaf1ac0311fdcc22899531823a1d9e96892ba1b
kolod <kolodina.lera322@mail.ru>
2021-11-07 20:19:25 +0800

    теперь графики идут отдельно, а не на одном графики

--- src/com/company/Controller.java
+++ src/com/company/Controller.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import javafx.application.Application;
+import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.chart.LineChart;
@@ -15,20 +16,20 @@ public class Controller extends Application {
 
     @Override
     public void start(Stage stage) {
-        stage.setTitle("Line Chart Sample");
-        //defining the axes
+        stage.setTitle("");
+        // определение осей
         final NumberAxis xAxis = new NumberAxis();
         final NumberAxis yAxis = new NumberAxis();
         xAxis.setLabel("Признаки");
-        //creating the chart
-        final LineChart<Number,Number> lineChart =
-                new LineChart<Number,Number>(xAxis,yAxis);
+        // создание графика
+        final LineChart<Number,Number> lineChart = new LineChart<Number,Number>(xAxis,yAxis);
 
         lineChart.setTitle("Признаки особи с наименьшим эвклидовым расстоянием в виде графика и график функции Y=ln(2x)");
         // Делаем график искомой функции
         XYChart.Series series1 = new XYChart.Series();
         series1.setName("Искомая функция");
 
+        // создаем популяцию с 20ю особями, по 100 признаков у каждой
         Population population = new Population(20, 100);
         population.createPopulation(); // заполняем популяцию особями
         double[] x = population.getxForRequiredFunc();
@@ -36,6 +37,9 @@ public class Controller extends Application {
         for (int i = 0; i < x.length; i++){
             series1.getData().add(new XYChart.Data(x[i], y[i]));
         }
+
+        // создание графика
+        final LineChart<Number,Number> lineChart2 = new LineChart<Number,Number>(xAxis,yAxis);
 
         // выполняем генетический алгоритм
         population.startGeneticAlg();
@@ -54,8 +58,14 @@ public class Controller extends Application {
         double euc = WorkWithNumbers.getEuclideanDistance(y, y2);
         System.out.println("euc=" + euc);
 
-        Scene scene  = new Scene(lineChart,800,600);
-        lineChart.getData().addAll(series1, series2);
+        FlowPane root = new FlowPane(lineChart, lineChart2);
+        Scene scene  = new Scene(root);
+
+        //Scene scene  = new Scene(lineChart, 800,600);
+        // lineChart.getData().addAll(series1, series2);
+
+        lineChart.getData().add(series1);
+        lineChart2.getData().add(series2);
 
         stage.setScene(scene);
         stage.show();
